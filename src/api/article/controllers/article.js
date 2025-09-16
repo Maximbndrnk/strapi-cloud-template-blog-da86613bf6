8bb4e3489d1702595ad9689277cae1406eade865
max-bondarenko-evo <max.bondarenko@evo-studio.com>
2025-09-16 22:38:41 +0300

    modify article find

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -6,4 +6,51 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::article.article', ({ strapi }) => ({
+  // Перевизначаємо метод find для додавання populate автора з аватаром
+  async find(ctx) {
+    const { query } = ctx;
+
+    // Додаємо populate для автора з аватаром
+    const entity = await strapi.entityService.findMany('api::article.article', {
+      ...query,
+      populate: {
+        ...query.populate,
+        author: {
+          fields: ['id', 'name', 'email'],
+          populate: {
+            avatar: {
+              fields: ['url', 'name', 'alternativeText'],
+            },
+          },
+        },
+      },
+    });
+
+    return entity;
+  },
+
+  // Перевизначаємо метод findOne для додавання populate автора з аватаром
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const { query } = ctx;
+
+    // Додаємо populate для автора з аватаром
+    const entity = await strapi.entityService.findOne('api::article.article', id, {
+      ...query,
+      populate: {
+        ...query.populate,
+        author: {
+          fields: ['id', 'name', 'email'],
+          populate: {
+            avatar: {
+              fields: ['url', 'name', 'alternativeText'],
+            },
+          },
+        },
+      },
+    });
+
+    return entity;
+  },
+}));
