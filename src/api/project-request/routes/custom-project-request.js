f81e45335f27ff6f6dc5287dd7fd0ff09f65892b
Maxim Bondarenko <maximb@runway.com.au>
2026-04-14 14:46:07 +0300

    add new project reques endpoint

---
+++ src/api/project-request/routes/custom-project-request.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  routes: [
+    {
+      method: 'PUT',
+      path: '/project-requests/:id/stepper-data',
+      handler: 'project-request.updateStepperData',
+      config: {}
+    }
+  ]
+};
